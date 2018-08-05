aa0ab11fccd2a1d8176cf4fea88bb773730ab6c9
Brent  Ransom <sadumedusa@gmail.com>
2018-08-05 12:13:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,16 @@ end
 
   end
 end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
